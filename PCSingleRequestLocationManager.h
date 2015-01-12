fce5fc6cb0d754092838a1c2646f00b02c4103a6
Matt Cheetham <matt@3sidedcube.com>
2015-01-12 08:36:23 +0000

    Adds better compatibility for iOS 8 and iOS 7.

--- PCSingleRequestLocationManager.h
+++ PCSingleRequestLocationManager.h
@@ -13,7 +13,19 @@
 
 @interface PCSingleRequestLocationManager : NSObject
 
+typedef NS_ENUM(NSInteger, PCAuthorizationType) {
+    PCAuthorizationTypeWhenInUse = 1,
+    PCAuthorizationTypeAlways = 2
+};
+
 typedef void (^PCSingleRequestLocationCompletion)(CLLocation *location, NSError *error);
+
+/**
+ Requests a users current location and fires a completion block once it has established that an accurate location has been found, or that an error has occured
+ @param authorization The PCAuthorizationType for the request. Defines whether the app can use location services in the background or not
+ @param completion The PCSingleRequestLocationCompletion block to be fired when the manager has found or failed to find the current location
+ **/
+- (void)requestCurrentLocationWithAuthorizationType:(PCAuthorizationType)authorization completion:(PCSingleRequestLocationCompletion)completion;
 
 /**
  Requests a users current location and fires a completion block once it has established that an accurate location has been found, or that an error has occured
